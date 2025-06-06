b1cd087cb6bed0e4fb6d4549cee633cf9c3d041f
yakudza992683 <mairbektemrokov@yandex.ru>
2025-06-06 21:38:14 +0000

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream>  // подключаем библиотеку для ввода/вывода
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name: ";  // просим ввести имя
     std::cin >> name;
-    std::cout << "Hello, world from " << name << "!";
+    std::cout << "Hello, world from " << name << "!";  // выводим приветствие
     return 0;
 }
